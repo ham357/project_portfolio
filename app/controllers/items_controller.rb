e468508dc67e2b12339e6d12b0aa3d63b0b73a5c
ham357 <sakuraniumarete@gmail.com>
2019-06-04 20:15:40 +0900

    add 投稿フォームの実装
    投稿フォームとcreateアクションの実装を行いました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,6 +8,24 @@ class ItemsController < ApplicationController
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+        respond_to do |format|
+            format.html { redirect_to root_path }
+            format.json
+        end
+    else
+        respond_to do |format|
+            format.html{render new_item_path}
+            format.json
+        end
+    end
+end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title,:description,:image).merge(user_id: 1)
   end
 
 end
