ac51a12211eaf52d6ac378f15694f68a905dbe50
ham357 <sakuraniumarete@gmail.com>
2019-06-05 14:42:59 +0900

    fix itemテストデータ
    テストデータのカラムを修正いたしました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
   factory :item do
     description        {"これはテスト。テストなのです。"}
       image            {File.open("#{Rails.root}/public/uploads/no_image.jpg")}
-    user_id
+    user
   end
 
 end
