37e423e31b27360e9b035a051b61a0eb311c5836
ham357 <sakuraniumarete@gmail.com>
2019-06-05 16:50:19 +0900

    add newアクション単体テスト
    newアクションの単体テストを作成いたしました。

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -16,4 +16,22 @@ describe ItemsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #new' do
+    it 'リクエストが成功すること' do
+      get :new
+      expect(response.status).to eq 200
+    end
+
+    it 'newテンプレートで表示されること' do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it '@itemがnewされていること' do
+      get :new
+      expect(assigns :item).to_not be_nil
+    end
+  end
+
 end
