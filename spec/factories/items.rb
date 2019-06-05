3681b1abb4f86bdb1ba9bc4e402d43d336f93bee
ham357 <sakuraniumarete@gmail.com>
2019-06-05 14:26:47 +0900

    fix itemテストデータ
    itemテストデータに画像ファイルのURLを追加しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
 
   factory :item do
     description        {"これはテスト。テストなのです。"}
-    image
+      image            {File.open("#{Rails.root}/public/uploads/no_image.jpg")}
     user_id
   end
 
